0a8c8dbfe2c44f090a6ff64bf69c503c557d541c
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-06-02 10:52:36 -0400

    Create htmlTemplates.py

---
+++ AppV1/htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://p7.hiclipart.com/preview/462/72/35/robot-the-terminator-skynet-sarah-connor-robot.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://i.pinimg.com/474x/2a/d5/f8/2ad5f8a792483e7f4524b35847477160.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
